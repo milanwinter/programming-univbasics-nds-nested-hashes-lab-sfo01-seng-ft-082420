e90d4f16ee290ece0badf39e77e9e87a24c521d7
Milan Winter <milanwinter8@gmail.com>
2020-07-31 21:01:39 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,11 +18,11 @@ end
 
 def literal_aoh
   this_array = [
-    DON_G.sort,
-    JOELLE_VD.sort,
-    PAT_M.sort,
-    KATE_G.sort,
-    BRUCE_G.sort
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
   ]
 end
 
